28e88e0cd8adcfb26e403f474ba00d74ae378653
Maxwell Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2022-02-06 14:50:09 -0700

    Add files via upload

---
+++ array.bat
@@ -0,0 +1,23 @@
+::This script was made by Max Fisher
+:mainScript
+	@if /I "%1%2%3%4%5%6%7%8%9" == "" (echo No input&exit /b) %= Error case for no input =%
+	@if /I "%1%2%3%4%5%6%7%8%9" == "/?" (call :helpPage&exit /b) else (@if "%1%2%3%4%5%6%7%8%9" == "help" call :helpPage&exit /b) %= Prints the help page on either `/?` or `help` =%
+	@if /I "%1%2%3%4%5%6%7%8%9" == "about" (call :aboutPage) %= Prints info about the script =%
+	@echo Invalid input&exit /b %= Error case for any invalid input =%
+@exit /b %= Forces a clean close if everything else somehow goes wrong =%
+
+
+
+:aboutPage
+	@echo Made by Max Fisher - @Maxwellcrafter
+	@echo Author's website: https://maxwellcrafter.com
+	@echo GitHub page: https://github.com/Maxwell-Fisher/BatchArrayHandler
+@exit /b
+
+
+
+:helpPage
+	@echo `/?`            Returns this help page
+	@echo `help`          Same as `/?`
+	@echo `about`         Returns info about this script
+@exit /b
