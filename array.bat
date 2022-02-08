1dd0a9b7ab4c6a0660671bea5f124c025d05961f
Maxwell Fisher <54481244+Maxwell-Fisher@users.noreply.github.com>
2022-02-08 14:56:38 -0700

    Add files via upload

--- array.bat
+++ array.bat
@@ -1,42 +1,63 @@
 ::This script was made by Max Fisher
+@setLocal enableDelayedExpansion
+
+
 
 :defineSetup
 	@set defaultIntArray=•0123456789aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ€‚ƒ„…†‡ˆ‰Š‹ŒŽ‘’“”–—˜™š›œžŸ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüý
 	@set defaultCharArray=•0123456789aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ€‚ƒ„…†‡ˆ‰Š‹ŒŽ‘’“”–—˜™š›œžŸ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüý
+	@set defaultArrayValue=••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
 ::
 
+
+
 :mainScript
-	@if /I "%1%2%3%4%5%6%7%8%9" == "" (echo No input&exit /b) %= Error case for no input =%
-	@if /I "%1" == "init" (call :initArray&exit /b) %= Initializes the array =%
-	@if /I "%1" == "remove" (call :removeArray&exit /b) %= Uninitializes the array to save memory =%
-	@if /I "%1" == "set" (call :setArray&exit /b) %= Sets the value of the array's selected index =%
-	@if /I "%1" == "read" (call :readArray&exit /b) %= Reads the array's selected index into a temp variable called arrayOut =%
-	@if /I "%1%2%3%4%5%6%7%8%9" == "/?" (call :helpPage&exit /b) else (@if "%1%2%3%4%5%6%7%8%9" == "help" call :helpPage&exit /b) %= Prints the help page on either `/?` or `help` =%
-	@if /I "%1%2%3%4%5%6%7%8%9" == "about" (call :aboutPage&exit /b) %= Prints info about the script =%
+	@if /I "%1%2%3%4%5%6%7%8%9" == "" (
+		echo No input&exit /b
+	) %= Error case for no input =%
+
+	@if /I "%1" == "init" (
+		set arrayTmpLength=%3&set %2=!defaultArrayValue:~0,%arrayTmpLength%!&exit /b
+	) %= Initializes the array =%
+
+	@if /I "%1" == "remove" (
+		call :arrayRemove&exit /b
+	) %= Uninitializes the array to save memory =%
+
+	@if /I "%1" == "set" (
+		@set "arrayTmp1=%2"&set "arrayTmp2=%3"&set "%arrayTmp1%=%arrayTmp2%"&echo %2&echo %3&exit /b
+	) %= Sets the value of the array's selected index =%
+
+	@if /I "%1" == "read" (
+		call :arrayRead&exit /b
+	) %= Reads the array's selected index into a temp variable called arrayOut =%
+
+	@if /I "%1%2%3%4%5%6%7%8%9" == "/?" (
+		call :helpPage&exit /b
+	) else (@if "%1%2%3%4%5%6%7%8%9" == "help" call :helpPage&exit /b) %= Prints the help page on either `/?` or `help` =%
+
+	@if /I "%1%2%3%4%5%6%7%8%9" == "about" (
+		call :aboutPage&exit /b
+	) %= Prints info about the script =%
+
 	@echo Invalid input&exit /b %= Error case for any invalid input =%
 @exit /b %= Forces a clean close if everything else somehow goes wrong =%
 
 
 
-:initArray
+:arrayInit
+	:code
+@exit /b
+
+
+
+:arrayRemove
 	::code
 @exit /b
 
 
 
-:removeArray
-	::code
-@exit /b
-
-
-
-:setArray
-	::code
-@exit /b
-
-
-
-:readArray
+:arrayRead
 	::code
 @exit /b
 
